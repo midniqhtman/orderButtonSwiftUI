da24f44944808d4e9cc2ccd8bc624619d652c864
midniqhtman <c1esi@mail.ru>
2022-12-27 22:33:19 +0300

    Initial Commit

---
+++ orderButtonSwiftUI/orderButtonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  orderButtonSwiftUIApp.swift
+//  orderButtonSwiftUI
+//
+//  Created by Байсаев Зубайр on 27.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct orderButtonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
